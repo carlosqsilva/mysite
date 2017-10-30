087b1e8f9815901e29693ca0ecd9c77f3b3624cf
Carlos Silva <carlosqsilva@outlook.com>
2017-10-30 13:08:28 -0300

    added profile picture

---
+++ src/sections/profile.js
@@ -0,0 +1,29 @@
+import { github, linkedin, instagram } from '../assets'
+
+const profile = {
+  name: "Carlos Henrique Silva",
+  repo: "https://github.com/carlosqsilva/mysite.git",
+  description: [
+    "Graduado pela UEPA, com diploma em Engenharia de Produção, atualmente faço Pos Graduação em Engenharia de Software.",
+    "Em meu tempo livre gosto de"
+  ],
+  social: [
+    {
+      title: "Github",
+      link: "https://github.com/carlosqsilva",
+      logo: github
+    },
+    {
+      title: "Linkedin",
+      link: "https://github.com/carlosqsilva",
+      logo: linkedin
+    },
+    {
+      title: "Instagram",
+      link: "https://github.com/carlosqsilva",
+      logo: instagram
+    }
+  ]
+}
+
+export default profile
