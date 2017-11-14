f9fb38f4146a146a79c2775a6eb6706c1d358494
Carlos Silva <carlosqsilva@outlook.com>
2017-11-14 17:49:22 -0300

    removed polyfills

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -56,7 +56,10 @@ module.exports = {
   // You can exclude the *.map files from the build during deployment.
   devtool: shouldUseSourceMap ? 'source-map' : false,
   // In production, we only want to load the polyfills and the app code.
-  entry: [require.resolve('./polyfills'), paths.appIndexJs],
+  entry: [
+    // require.resolve('./polyfills'), 
+    paths.appIndexJs
+  ],
   output: {
     // The build folder.
     path: paths.appBuild,
@@ -310,15 +313,7 @@ module.exports = {
       runtimeCaching: [
         {
           urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
-          handler: 'cacheFirst'
-        },
-        {
-          urlPattern: /^https:\/\/cf-media\.sndcdn\.com\//,
-          handler: 'cacheFirst'
-        },
-        {
-          urlPattern: /^https:\/\/api.soundcloud\.com\/tracks\//,
-          handler: 'cacheFirst'
+          handler: 'cacheFirst',
         }
       ],
       // For unknown URLs, fallback to the index page
