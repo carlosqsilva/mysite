1b0af44d48262a7d8e902aa6130355b38a656271
Carlos Silva <carlosqsilva@outlook.com>
2017-11-16 12:34:40 -0300

    added MediaSession api functionality

--- src/player.js
+++ src/player.js
@@ -7,7 +7,7 @@ class Player extends Component {
     this.client_id = "x3d1i5dxXwTtUNJAy8djMDh7yYdxSZX0"
     this.playlist = "https://soundcloud.com/carlos-silva-527/sets/website"
     this.state = {
-      isPlaying: true,
+      isPlaying: false,
       title: "",
       titleUrl: "",
       user: "",
@@ -15,8 +15,8 @@ class Player extends Component {
     }
     this.audio = null
     this.song = null
-    this.songs = null    
-    this.volume = 1
+    this.songs = null
+    this.constrols = "mediaSession" in navigator
 
     this.loadPlaylist = this.loadPlaylist.bind(this)
     this.play = this.play.bind(this)
@@ -24,40 +24,59 @@ class Player extends Component {
     this.playPrevious = this.playPrevious.bind(this)
     this.toggle = this.toggle.bind(this)
     this.isPlaying = this.isPlaying.bind(this)
+    this.shouldPlay = this.shouldPlay.bind(this)
   }
 
   componentDidMount() {
     this.audio = new Audio()
     this.audio.crossOrigin = 'anonymous'  
-    this.loadPlaylist(() => this.play())
+    this.loadPlaylist(() => this.shouldPlay())
+
+    if (this.constrols) {
+      navigator.mediaSession.playbackState = "Carregando media..."
+      navigator.mediaSession.setActionHandler('previoustrack', this.playPrevious)
+      navigator.mediaSession.setActionHandler('play', this.toggle)
+      navigator.mediaSession.setActionHandler('pause', this.toggle)
+      navigator.mediaSession.setActionHandler('nexttrack', this.playNext)
+    }
 
     this.audio.addEventListener("ended", this.playNext)
     this.audio.addEventListener("pause", this.isPlaying)    
     this.audio.addEventListener("play", this.isPlaying)
+  }
 
-    window.addEventListener("keydown", (e) => {
-      switch(e.key) {
-        case " ":
-          this.toggle()
-          break;
-        case 'ArrowRight':
-          this.playNext()
-          break;
-        case "ArrowLeft":
-          this.playPrevious()
-          break;
-        case "ArrowUp":
-          this.volume = ((this.volume + 0.1) >= 1) ? 1 : this.volume + 0.1
-          this.audio.volume = this.volume
-          break;
-        case "ArrowDown":
-          this.volume = ((this.volume - 0.1) <= 0) ? 0 : this.volume - 0.1
-          this.audio.volume = this.volume
-          break;
-        default:
-          break;
+  async shouldPlay() {
+    // Conditions
+    let battery = false
+    let memory = false
+    let connection = false
+
+    // don play if battery is charging or bellow 25%
+    if ("getBattery" in navigator) {
+      const batt = await navigator.getBattety()
+      if (!batt.charging && batt.level < 0.25) {
+        battery = true
       }
-    })
+    }
+
+    // dont play if memory device is bellow 1Gb
+    if ("deviceMemory" in navigator) {
+      if (navigator.deviceMemory < 1) {
+        memory = true
+      }
+    }
+
+    // dont play if network is limited
+    if ("connection" in navigator) {
+      if (navigator.connection.type === "cellular") {
+        connection = true
+      }
+    }
+
+    if (battery || memory || connection) {
+      return null
+    }
+    this.play()
   }
 
   loadPlaylist(callBack = null) {
@@ -73,6 +92,7 @@ class Player extends Component {
             titleUrl: track.permalink_url,
             user: track.user.username,
             userUrl: track.user.permalink_url,
+            artwork: track.artwork_url,
             stream: track.stream_url
           }
         })
@@ -90,18 +110,31 @@ class Player extends Component {
     }
 
     let currentSong = this.songs[song]
-    this.setState({
-      titleUrl: currentSong.titleUrl,
-      title: currentSong.title,
-      userUrl: currentSong.userUrl,
-      user: currentSong.user,
-      isPlaying: true
-    })
-
+    
     if (currentSong.stream) {
       this.audio.src = `${currentSong.stream}?client_id=${this.client_id}`
     }
-
+    
+    if (this.constrols) {
+      /* eslint-disable */
+      navigator.mediaSession.metadata = new MediaMetadata({
+        title: currentSong.title,
+        artist: currentSong.user,
+        album: currentSong.title,
+        artwork: [
+          { src: currentSong.artwork, sizes: '100x100', type: 'image/png' },
+        ]
+      })
+      /* eslint-enable */
+    } else {
+      this.setState({
+        titleUrl: currentSong.titleUrl,
+        title: currentSong.title,
+        userUrl: currentSong.userUrl,
+        user: currentSong.user,
+      })
+    }
+    
     this.audio.play()
     this.song = song
   }
@@ -131,23 +164,26 @@ class Player extends Component {
   }
 
   render() {
-    return (
-      <div className="playerWrapper">
-        <div className="playerButtons">
-          <button onClick={this.playPrevious} ><img src={previous} alt="" /></button>
-          <button onClick={this.toggle} ><img src={this.state.isPlaying ? pause : play} alt="" /></button>
-          <button onClick={this.playNext} ><img src={next} alt="" /></button>
+    if (!this.constrols) {
+      return (
+        <div className="playerWrapper">
+          <div className="playerButtons">
+            <button onClick={this.playPrevious} ><img src={previous} alt="" /></button>
+            <button onClick={this.toggle} ><img src={this.state.isPlaying ? pause : play} alt="" /></button>
+            <button onClick={this.playNext} ><img src={next} alt="" /></button>
+          </div>
+          <div className="playerInfo">
+            <a href="https://soundcloud.com" >
+              <img src={soundcloud} alt="" /> SOUNDCLOUD -
+            </a>
+            <a href={this.state.titleUrl}>{this.state.title}{" "}</a>
+            by
+            <a href={this.state.userUrl}>{" "}{this.state.user}</a>
+          </div>
         </div>
-        <div className="playerInfo">
-          <a href="https://soundcloud.com" >
-            <img src={soundcloud} alt="" /> SOUNDCLOUD -
-          </a>
-          <a href={this.state.titleUrl}>{this.state.title}{" "}</a>
-          by
-          <a href={this.state.userUrl}>{" "}{this.state.user}</a>
-        </div>
-      </div>
-    )
+      )
+    }
+    return null
   }
 }
 
